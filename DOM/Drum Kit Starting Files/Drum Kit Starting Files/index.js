ed7537bb6293f2589c186f0133f5c99ccca75499
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-06-22 22:05:02 +0800

    Drum Kit
    138-139

--- DOM/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ DOM/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,7 +1,17 @@
 
-document.querySelector("button").addEventListener("click", handleClick);
-//when using parenthesis, it will become a method so functions are executed
-function handleClick ()
+// document.querySelector("button").addEventListener("click", handleClick);
+// //when using parenthesis, it will become a method so functions are executed
+// function handleClick ()
+// {
+//     alert("I got clicked");
+// }
+
+// can use ."class" for querySelector
+var NumberofButtons =  document.querySelectorAll(".drum").length;
+for(var i = 0; i < NumberofButtons ;i++)
 {
-    alert("I got clicked");
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        alert("I got clicked");
+    });
 }
+
