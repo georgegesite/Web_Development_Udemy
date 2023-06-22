8ae94775395d3ce16211e73dd0fca06b818416cc
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-06-22 21:23:46 +0800

    Drum Kit
    137

---
+++ DOM/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+
+document.querySelector("button").addEventListener("click", handleClick);
+//when using parenthesis, it will become a method so functions are executed
+function handleClick ()
+{
+    alert("I got clicked");
+}
