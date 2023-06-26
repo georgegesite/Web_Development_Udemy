64410f682148e1b0c8e2b8eae74a19b3f2895480
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-06-26 21:52:37 +0800

    jQuery
    151, 152

---
+++ JQUERY/script.js
@@ -0,0 +1,8 @@
+$("h1").css("color", "red");
+
+
+// $(document).ready(function  () {
+//     $("h1").css("color", "red");
+// })
+
+// use code above is jquery CDN is linked at the header and not on the body
