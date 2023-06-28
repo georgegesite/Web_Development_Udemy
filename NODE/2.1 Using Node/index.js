ec9e0be0fe32b9d2b086be71d244883cca211592
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-06-28 13:36:20 +0800

    Node
    196

---
+++ NODE/2.1 Using Node/index.js
@@ -0,0 +1,6 @@
+// Node REPL
+// Read Evaluate Print Loop - is a computer environment where user inputs are read and evaluyated, and then the results are returned to the user
+console.log("Hello and Welcomde to Node");
+
+// Native Node Modules
+// File System  CJS | ESM
