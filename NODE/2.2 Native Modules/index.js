6e8f76ac98dc1388c380bbe460865acda2c6b9ec
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-06-30 22:48:24 +0800

    Node JS
    197,198

--- NODE/2.2 Native Modules/index.js
+++ NODE/2.2 Native Modules/index.js
@@ -1,1 +1,8 @@
-//BUSY TODAY SO NOT CODING
+
+//native node modules "File System"
+const fs = require("fs");
+
+fs.writeFile("message.txt", "Hello From Node JS!", (err) => {
+if (err) throw err;
+console.log("The file has been saved");
+});
