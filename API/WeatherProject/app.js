94837fd767477a8ad83427400c40705ffd7ba90b
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-07-08 21:05:39 +0800

    Weather API with user Query
    221

--- API/WeatherProject/app.js
+++ API/WeatherProject/app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+const bodyParser = require("body-parser");
 
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req,res){
-
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Manila,PH&appid=7998f1cba9e81c3f25c814057f77f5d8"
+    res.sendFile(__dirname + "/index.html");
+    
+})
+app.post("/",function(req,res)
+{
+    console.log("Working");
+    const query = req.body.cityName
+    const APIkey = "7998f1cba9e81c3f25c814057f77f5d8"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+APIkey+""
     https.get(url , function(response)
     {
         console.log(response.statusCode);
@@ -26,14 +36,14 @@ app.get("/", function(req,res){
             const icon = weatherData.weather[0].icon;
             // console.log(temp);
             const imageURL = "https://openweathermap.org/img/wn/"+icon+"@2x.png";
-            res.write("<h1> The description of Manila is " + weatherDescription +".</h1>");
-            res.write("<h1> The temperature of Manila is " + temp +" Kelvin.</h1>");
+            res.write("<h1> The description of "+query+" is " + weatherDescription +".</h1>");
+            res.write("<h1> The temperature of "+query+" is " + temp +" Kelvin.</h1>");
             res.write("<img src ="+ imageURL+ ">");
             res.send()
         })
     })
-  
 })
+
 app.listen (3000, function (){
     console.log("server is running on port 3000");
 })
