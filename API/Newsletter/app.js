f4f27de89bef9dcc9db13e49e4e124947917ea16
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-07-09 17:01:32 +0800

    Update app.js
    User data transferred to API

--- API/Newsletter/app.js
+++ API/Newsletter/app.js
@@ -4,21 +4,50 @@ const requrest = require("request");
 const https = require("https");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(express.static("public"))
 
-app.get("/", function(req,res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 })
 
-app.post("/", function (req,res)
-{   
-var firstName = req.body.fname;
-var lastName = req.body.lname;
-var eMail = req.body.email;
+app.post("/", function (req, res) {
+    const firstName = req.body.fname;
+    const lastName = req.body.lname;
+    const eMail = req.body.email;
 
-console.log(firstName, lastName, eMail);
+
+    const data = {
+        members: [{
+            email_address: eMail,
+            status: 'subscribed', // "subscribed" or "unsubscribed". defaults to subscribed if not provided
+            merge_fields: {
+                fname: firstName,
+                lname: lastName,
+            }
+        }]
+    }
+    const jsonData = JSON.stringify(data);
+    console.log(jsonData);
+    const url = 'https://us21.api.mailchimp.com/3.0/lists/087eca132c'
+    const options = {
+        method: "POST",
+        auth: "georgedeveloper:2ed658003883d561ae72f3e57fb25a0b-us21",
+    }
+    const request = https.request(url, options, function (response) {
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
 })
-app.listen (3000, function (){
+app.listen(3000, function () {
     console.log("server is running on port 3000");
 })
+
+// API KEY: 2ed658003883d561ae72f3e57fb25a0b-us21
+// ID 087eca132c
