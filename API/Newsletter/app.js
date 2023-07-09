cae7257468f886eb791fd28a0d19485250cc3e45
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-07-09 16:33:34 +0800

    Newsletter
    223 sign up page

--- API/Newsletter/app.js
+++ API/Newsletter/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const app = express();
-const https = require("https");
 const bodyParser = require("body-parser")
+const requrest = require("request");
+const https = require("https");
+
+const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
 
 app.get("/", function(req,res) {
     res.sendFile(__dirname + "/signup.html");
 })
 
+app.post("/", function (req,res)
+{   
+var firstName = req.body.fname;
+var lastName = req.body.lname;
+var eMail = req.body.email;
+
+console.log(firstName, lastName, eMail);
+})
 app.listen (3000, function (){
     console.log("server is running on port 3000");
 })
