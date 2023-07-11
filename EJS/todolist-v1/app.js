735aca63a954936c66548dc296b6f2ee59777b44
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-07-11 21:31:12 +0800

    EJS
    creating templates

--- EJS/todolist-v1/app.js
+++ EJS/todolist-v1/app.js
@@ -1,27 +1,50 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser =require("body-parser");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.get("/", function(req, res){ // request first, then response
-    var today = new Date();
-    if (today.getDay() === 6 || today.getDay() === 0) {
-        // res.write("<h1> Yay its the weekend </h1>");
-        // res.send();
-        // res.sendFile(__dirname + "/index.html");
-    } else {
-        // res.write("<h1> It's not a weekend </h1>");
-        // res.write("<h1> Boo i have to work </h1>");
-        // res.send();
-        res.sendFile(__dirname + "/index.html");
-    }
+app.set('view engine', 'ejs');
 
+app.get("/", function (req, res) { // request first, then response
+    var today = new Date();
+    var currentDay = today.getDay();
+    console.log(currentDay);
+    var day = "";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error");
+            break;
+    }
+    res.render('list', {
+        kindOfDay: day
+    });
     // res.send("Hello World");
     // res.sendFile(__dirname + "/index.html");
+    // Hello world
 });
 
-app.listen(3000, function (){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
