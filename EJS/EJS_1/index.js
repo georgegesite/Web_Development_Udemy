26daf29d543c943ca73595990fc3a4ae8567dbe0
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-09 07:51:42 +0800

    210 EJS Tags
    done it very ez

--- EJS/EJS_1/index.js
+++ EJS/EJS_1/index.js
@@ -3,18 +3,19 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+
     const today = new Date();
     const day = today.getDay();
-    console.log(day);
-
+    // console.log(day);
 
     let type = "a weekday";
     let adv = "it's time to work hard";
 
-    if (day === 0 || day === 6){
+    if (day === 0 || day === 6) {
         let type = "a weekend";
         let adv = "it's time to rest";
     }
+
     res.render("index.ejs", {
         dayType: type,
         advice: adv
