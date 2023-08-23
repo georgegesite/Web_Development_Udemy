31732c709f832b8dbbdd0f2b742c1c57920dcb61
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-23 21:49:44 +0800

    added twosum to website

--- EJS/nodeEXpress/index.js
+++ EJS/nodeEXpress/index.js
@@ -1,19 +1,66 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
+const port = 3000;
 
 app.set('view engine','ejs');
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req,res) =>{
-    const student ={
-        name: "Mohammad",
-        age:"20" ,
-        email : "mohammad@gmail.com",
-    }
-    res.render("Home.ejs",{student: student});
+    res.render("Home.ejs");
 })
 
 app.get("/about", (req,res) =>{
     res.render("about.ejs");
 })
-app.listen(4000);
+
+let bowl = ["Apples", "Oranges", "Peers"];
+
+app.get("/fruits", (req, res) =>{
+    res.render("fruits.ejs", {fruits: bowl});
+})
+
+
+app.get("/validparenthesis", (req,res) =>{
+    res.render("validparenthesis.ejs")
+});
+app.post("/validparenthesissubmit", (req, res) => {
+    var s = req.body["string"];
+    var aanswer = isValid(s);
+    if(aanswer == true)
+    var answer = "TRUE"
+    else
+    var answer = "FALSE"
+    res.render("validparenthesis.ejs", { FinalAnswer: answer, sstring: s });
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+
+var isValid = function(s) {
+    const hashmap = {
+        "(": ")",
+        "[": "]",
+        "{": "}"
+    };
+    const stack = [];
+
+    if (s === "" || s.length % 2 !== 0) {
+        return false;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] in hashmap) {
+            stack.push(s[i]);
+        } else {
+            let topElement = stack.pop();
+            if (hashmap[topElement] !== s[i]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+};
