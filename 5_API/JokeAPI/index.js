7bef1e1a5985dcb7fcb542586d68b2cd71bda3ef
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-24 16:20:29 +0800

    Joke api worked

---
+++ 5_API/JokeAPI/index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import axios from'axios'
+import bodyParser from 'body-parser'
+
+const app = express();
+const port = 3000;
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", async (req,res) =>{
+    res.render("index.ejs")
+});
+
+app.post("/submit", async (req, res) => {
+    try {
+    const category = req.body.answer;
+      const result = await axios.get(`https://v2.jokeapi.dev/joke/${category}?type=single`);
+      console.log(result)
+      res.render("index.ejs", {
+        theJoke: result.data.joke,
+      });
+    } catch (error) {
+      if (error.response && error.response.data) {
+        console.log(error.response.data);
+      } else {
+        console.log("An error occurred:", error.message);
+      }
+      res.status(500).send("An error occurred.");
+    }
+  });
+  
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
