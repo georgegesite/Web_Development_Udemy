16d0d2d2f066cafc0fcc8eab14a6174a5ef6bb54
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-28 21:21:21 +0800

    Update people.js

--- NoSQL/FruitsProjects/people.js
+++ NoSQL/FruitsProjects/people.js
@@ -7,9 +7,12 @@ async function main() {
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-
+//Create
 const peopleSchema = new mongoose.Schema({
-    name: String,
+    name: {
+        type: String,
+        required: [true, 'Name Required']
+      },
     age: Number,
 })
 
@@ -27,10 +30,21 @@ const choco = new People({
     name: 'Choco Bear',
     age: 4,
 });
+const freak = new People({
+    name:"Freak",
+    age:10
+})
+// INSERT TO DB
+freak.save();
 
-// People.insertMany([george, flom, choco]);
 
-async function shwoDocuments() {
+
+
+// People.insertMany([george, flom, freak]);
+
+
+//Read 
+async function showDocuments() {
     const ppl = await People.find();
     // console.log(ppl);
     mongoose.connection.close();
@@ -39,4 +53,30 @@ async function shwoDocuments() {
     })
 
 }
-shwoDocuments();
+
+
+// Update Data
+
+async function UpdateName()
+{
+    await People.updateOne({ _id: "64ec86ca31c54c9df3bcc0a5" }, { name: 'Flom and George' });
+}
+// UpdateName();
+
+
+//Delete Data
+var fname = "George Gesite";
+async function deletePeople(){
+    await People.deleteOne({name: fname });
+    console.log(`Sucessfully deleted ${fname}`);
+}
+// deletePeople();
+
+async function deleteManyPeople()
+{
+    // await Character.deleteMany({ name: /Stark/, age: { $gte: 18 } }); /
+    await People.deleteMany({name: fname});
+    console.log(`Sucessfully deleted ${fname}`);
+}
+// deleteManyPeople();
+showDocuments();
