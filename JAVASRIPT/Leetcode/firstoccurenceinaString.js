c9ffcdb2993c8e0da6cca75c96d639c6c8e82fd1
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-29 21:58:09 +0800

    Leetcode

---
+++ JAVASRIPT/Leetcode/firstoccurenceinaString.js
@@ -0,0 +1,31 @@
+var haystack = "hello"
+var needle = "ll"
+
+// var s = haystack.slice(0,needle.length);
+// console.log(s);
+// var s = haystack.slice(1,needle.length+1);
+// console.log(s);
+// var s = haystack.slice(2,needle.length+2);
+// console.log(s);
+// var s = haystack.slice(3,needle.length+3);
+// console.log(s);
+// var s = haystack.slice(4,needle.length+4);
+// console.log(s);
+
+
+var strStr = function(haystack, needle) {
+
+    for(let i = 0; i <haystack.length;i++)
+    {
+        var s = haystack.slice(i,needle.length+i);
+        if(needle == s) {
+            return i;
+            break;
+        }
+
+
+    }
+    return -1;
+};
+
+console.log(strStr(haystack,needle));
