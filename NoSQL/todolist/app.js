92db1cf53ad48c4a2fb988de41b322728d862e3d
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-08-29 21:01:16 +0800

    Todolist Complete
    Todolist using:
    EJS, NODE & MONGOOSE

---
+++ NoSQL/todolist/app.js
@@ -0,0 +1,125 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/todolistdb', {
+    useNewUrlParser: true
+  }); //todolistdb = name of databse
+}
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+const todoSchema = new mongoose.Schema({ // todolist schema
+  name: {
+    type: String,
+    required: [true, 'Name Required']
+  },
+})
+
+const workSchema = new mongoose.Schema({ // Worktodolist schema
+  name: {
+    type: String,
+    required: [true, 'Name Required']
+  },
+})
+
+const Todo = mongoose.model('todolist', todoSchema);
+const WorkTodo = mongoose.model('worktodolist', workSchema)
+
+
+
+async function requestTask() {
+  const tasks = await Todo.find();
+  tasks.forEach(function (task) {
+    items.push(task.name);
+    console.log(task.name);
+  })
+}
+async function requestWorkTask() {
+  const tasks = await WorkTodo.find();
+  tasks.forEach(function (task) {
+    workItems.push(task.name);
+    console.log(task.name);
+  })
+}
+var items = []; // get data from database
+var workItems = [];
+requestWorkTask();
+requestTask();
+app.get("/", function (req, res) {
+
+  const day = date.getDate();
+  res.render("list", {
+    listTitle: day,
+    newListItems: items
+  });
+
+});
+
+app.post("/", function (req, res) {
+
+  const item = req.body.newItem; // get item from input
+  const newItem = new Todo({ // create object
+    name: item,
+  });
+  items.push(newItem.name); // save into array
+  newItem.save(); // insert into database
+  console.log("Task Saved");
+  res.redirect("/");
+
+});
+
+app.post("/delete", function (req, res) {
+
+  console.log(req.body.checkbox);
+  const deleteItem = req.body.checkbox
+  async function deletePeople() {
+    await Todo.deleteOne({
+      name: deleteItem
+    });
+    console.log(`Sucessfully deleted ${deleteItem}`);
+  }
+  items = [];
+  requestTask();
+  deletePeople();
+  res.redirect("/");
+
+})
+
+
+app.get("/work", function (req, res) {
+
+
+  const item = req.body.newItem; // get item from input
+  const newWorkItem = new WorkTodo({ // create object
+    name: item,
+  });
+  workItems.push(newWorkItem.name); // save into array
+  newWorkItem.save(); // insert into database
+  console.log("Work Task Saved");
+  res.redirect("/work");
+
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
