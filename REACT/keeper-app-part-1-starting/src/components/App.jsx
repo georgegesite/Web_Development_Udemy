11c160ba0e7995a97ce10d12cdf3dcfe9b317e47
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-09-06 15:23:39 +0800

    React update v2

--- REACT/keeper-app-part-1-starting/src/components/App.jsx
+++ REACT/keeper-app-part-1-starting/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import tasks from "../tasks";
 
 function App()
 {
@@ -9,9 +10,9 @@ function App()
     <div>
             <Header/>
             <Footer />
-            <Note title="Study" content="Learn DSA & OOP"/>
-            <Note title="Review" content="LeetCode and JS Syntax"/>
-        </div>
+            <Note title={tasks[0].title} content={tasks[0].content}/>
+            <Note title={tasks[1].title} content={tasks[1].content}/>
+    </div>
     )
 };
 
---
+++ REACT/keeper-app-part-1-starting/src/tasks.jsx
@@ -0,0 +1,38 @@
+const tasks = [
+    {
+      title: "Buy groceries",
+      content: "Milk, eggs, bread",
+    },
+    {
+      title: "Call mom",
+      content: "Wish her a happy birthday and ask about her day",
+    },
+    {
+      title: "Finish the report",
+      content: "Due by Friday, proofread and double-check for errors",
+    },
+    {
+      title: "Plan vacation",
+      content: "Research destinations and book flights",
+    },
+    {
+      title: "Go to the gym",
+      content: "Cardio and weightlifting",
+    },
+    // Add more tasks below
+    {
+      title: "Read a book",
+      content: "Choose a book and dedicate an hour to reading",
+    },
+    {
+      title: "Pay bills",
+      content: "Utility bills, rent, and credit card payment",
+    },
+    {
+      title: "Write code",
+      content: "Work on a coding project for at least 2 hours",
+    },
+  ];
+  
+export default tasks;
+  
