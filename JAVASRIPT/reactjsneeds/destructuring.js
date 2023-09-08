bc8299f207b3d8a970d366d4fbcbfb26f54aa2e7
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-09-08 20:39:17 +0800

    React Module

---
+++ JAVASRIPT/reactjsneeds/destructuring.js
@@ -0,0 +1,41 @@
+
+
+const cars = [
+    {
+      model: "Honda Civic",
+      coloursByPopularity: ["black", "silver"],
+      speedStats: {
+        topSpeed: 140,
+        zeroToSixty: 8.5
+      }
+    },
+    {
+      model: "Tesla Model 3",
+      coloursByPopularity: ["red", "white"],
+      speedStats: {
+        topSpeed: 150,
+        zeroToSixty: 3.2
+      }
+    }
+];  
+
+const [honda, tesla] = cars;
+
+const {
+  speedStats: { topSpeed: hondaTopSpeed }
+} = honda;
+const {
+  speedStats: { topSpeed: teslaTopSpeed }
+} = tesla;
+
+const {
+  coloursByPopularity: [hondaTopColour]
+} = honda;
+const {
+  coloursByPopularity: [teslaTopColour]
+} = tesla;
+
+console.log(honda);
+console.log(honda.model);
+console.log(hondaTopSpeed);
+console.log(hondaTopColour);
